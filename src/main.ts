85a9fae7b58a0efef5d95e6d14d6d34c3a4a987b
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2024-02-08 14:01:01 +0700

    iniitial commit

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import { engines } from '../package.json';
+import { satisfies } from 'semver';
+import { VersioningType } from '@nestjs/common';
+
+async function bootstrap() {
+  const nodeVersion = engines.node;
+  if (!satisfies(process.version, nodeVersion)) {
+    console.log(
+      `Required node version ${nodeVersion} not satisfied with current version ${process.version}.`,
+    );
+    process.exit(0);
+  }
+
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.setGlobalPrefix('api');
+}
+bootstrap();
